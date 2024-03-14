6e31240a9031a5b0bc5fe00b91e3fa39ec732516
fy201 <160870635+fy201@users.noreply.github.com>
2024-03-14 10:15:33 +0000

    to test o number is Even complete

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,25 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static boolean isEven(int n){
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number:");
+        int userNumber = scanner.nextInt();
+        System.out.println("is the number even?" + isEven(userNumber));
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -15,6 +16,15 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue(App.isEven(0) );
+        assertTrue(App.isEven(2));
+        assertTrue(App.isEven(40));
+    }
+    
+    @Test
+    public void shouldAnswerWithFalse() {
+        assertFalse(App.isEven(1));
+        assertFalse(App.isEven(15));
+        assertFalse(App.isEven(-31));
     }
 }
